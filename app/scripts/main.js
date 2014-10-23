73acba3f22c571682b641f495a84f771d93d6fab
Kaelig <kaelig@deloumeau.fr>
2014-10-23 12:03:11 +0100

    Show / Hide filter bar

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,24 @@
 'use strict';
 
 onDomReady(function () {
+  if ('querySelector' in document &&
+      'localStorage' in window &&
+      'addEventListener' in window &&
+      'classList' in document.createElement('_')) {
+    document.documentElement.className = document.documentElement.className.replace(/\bno-js\b/g, 'js');
 
-  require('fastclick')(document.body); // github.com/ftlabs/fastclick
+    require('fastclick')(document.body); // github.com/ftlabs/fastclick
 
-  /* ADD INTERACTIVITY HERE! */
+    var filterBarToggleElement = '<a href="#filterbar" class="js-filter-bar-toggle top-bar__action" role="button"><span class="visuallyhidden">Menu</span></a>';
+    document.querySelector('.js-filter-bar-toggle-placeholder').innerHTML = filterBarToggleElement;
 
+    var filterBar = document.querySelector('.js-filter-bar');
 
-
-
-
-
-
-
-
-
-
-  // Now unhide everything by removing the `invisible` class from the body
-  document.body.className = document.body.className.replace(/\binvisible\b/, '');
+    [].forEach.call(document.querySelectorAll('.js-filter-bar-toggle'), function (el) {
+      el.addEventListener('click', function(e) {
+        e.preventDefault();
+        filterBar.classList.toggle('filter-bar--hidden');
+      }, false);
+    });
+  }
 });
